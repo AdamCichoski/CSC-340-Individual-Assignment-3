2af4aa321c0eb287bb1b94c57843eff129346619
Adam Cichoski <cjadam003@gmail.com>
2024-09-19 17:31:02 -0400

    Each page contains: images; at least 1 business related text segment; links to the other pages; a business logo in the menu bar.
    Required items: Company banner

--- src/main/java/com/csc340/individualAssignment3/MainController.java
+++ src/main/java/com/csc340/individualAssignment3/MainController.java
@@ -7,16 +7,35 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MainController {
 
+    /**
+     * Mapping to the home page
+     * Displays the page containing a general welcome to the company
+     * @return index html file
+     */
     @GetMapping("/home")
     public String homePage(){
         return "index";
     }
 
+    /**
+     * Mapping to the about page
+     * Displays the page containing information about the company
+     * Our Story
+     *
+     * @return about html
+     */
     @GetMapping("/about")
     public String aboutPage(){
         return "about";
     }
 
+    /**
+     * Mapping to the contact page
+     * Displays the page containing information about the company
+     * Scheduling an appointment
+     * Founder image
+     * @return
+     */
     @GetMapping("/contact")
     public String contactPage(){
         return "contact";
